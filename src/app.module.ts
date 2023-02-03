b5adef37f1e8bc86925c34b9bcdeecd5ca931bfc
Lars Juul Langgaard <dkLaJuLa@a-flyfq13.corp.lego.com>
2023-02-03 19:43:06 +0100

    Adding more

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RabbitMQModule } from './infrastructure/rabbit-mq/rabbit-mq.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [RabbitMQModule],
+  imports: [ConfigModule.forRoot(), RabbitMQModule],
   controllers: [AppController],
   providers: [AppService],
 })
