0f9e5ff08b2494b8725955fccbc5fbb05fc0977f
Lars Juul Langgaard <dkLaJuLa@a-flyfq13.corp.lego.com>
2023-02-03 21:37:26 +0100

    trying to fix error

--- src/infrastructure/rabbit-mq/rabbit-mq.module.ts
+++ src/infrastructure/rabbit-mq/rabbit-mq.module.ts
@@ -1,17 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { RabbitMQService } from './rabbit-mq.service';
-import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
-    ConfigModule.forRoot(),
     ClientsModule.register([
       {
         name: 'rabbit-mq-module',
         transport: Transport.RMQ,
         options: {
           urls: [
-            `amqps://${process.env.PRODUCER_USERNAME}:${process.env.PRODUCER_PASSWORD}@b-902ad621-f4ca-4d73-88e5-42bb5540f386.mq.eu-west-1.amazonaws.com:5671`,
+            `amqps://travelerproducer:fydrit-dabxa4-Jadtox@b-902ad621-f4ca-4d73-88e5-42bb5540f386.mq.eu-west-1.amazonaws.com:5671`,
           ],
           queue: 'rabbit-mq-nest-js',
         },
