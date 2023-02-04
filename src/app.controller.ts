732cb227a8b2ff915663b291fbb5ee876db2803b
Lars Juul Langgaard <dkLaJuLa@a-flyfq13.corp.lego.com>
2023-02-04 08:24:19 +0100

    trying to fix error

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,21 +1,24 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Ctx, MessagePattern, Payload, RmqContext } from "@nestjs/microservices";
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    // private readonly rabbitMQService: RabbitMQService,
-  ) {}
+  constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello() {
-    // noinspection JSIgnoredPromiseFromCall
-    /*this.rabbitMQService
-      .send('rabbit-mq-producer', {
-        message: this.appService.getHello(),
-      })
-      .subscribe();*/
-    return 'message sent';
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @MessagePattern('rabbit-mq-producer')
+  public async execute(
+    @Payload() data: any,
+    @Ctx() context: RmqContext
+  ) {
+    const channel = context.getChannelRef();
+    const orginalMessage = context.getMessage();
+    console.log('data', data);
+    channel.ack(orginalMessage);
   }
 }
