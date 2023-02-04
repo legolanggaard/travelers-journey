eb6dbb48d021e3c4b9a42d1b64fcb8652ce3249d
Lars Juul Langgaard <dkLaJuLa@a-flyfq13.corp.lego.com>
2023-02-04 06:33:53 +0100

    trying to fix error

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
+  console.log('listening on port:', port)
   await app.listen(port);
 }
-bootstrap().then();
+bootstrap();
